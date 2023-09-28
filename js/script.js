4e1f9959434f1a1d7bd595eb836d0cc11cad145a
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-09-28 10:05:50 +0500

    lesson-2

---
+++ js/script.js
@@ -0,0 +1,83 @@
+// const firstName = 'Muhammadrasul';
+// const age = '15';
+
+// if (typeof age == 'number') {
+//     console.log('number type kiritildi');
+// } else if (typeof age === 'string') {
+//     console.log('string type');
+// } else {
+//     console.log('boshqa type');
+// }
+
+// const myNumber = 7.5;
+// console.log(Math.round(myNumber));
+
+// const myRandomNumber = Math.round(Math.random() * (80 - 70) + 70 + 1);
+// console.log(myRandomNumber);
+
+// const myFixed = 5.999;
+
+// console.log(myFixed.toFixed());
+
+// const myNumber = false;
+// const convertedNumber = Number(myNumber);
+
+// console.log(convertedNumber);
+
+// const myType = "Men" - 23 - "yoshdaman";
+
+// console.log(myType);
+
+// let n = 1 + '1';
+// n = n - 1;
+
+// let money;
+
+// if (money) {
+//     console.log('bor');
+// } else {
+//     console.log('yoq');
+// }
+
+// const isDriverLicence = true;
+// const isVisible = false;
+
+// if (isDriverLicence || isVisible) {
+//     console.log('mumkin');
+// } else {
+//     console.log('mumkin emas');
+// }
+
+// const myPrompt = parseInt(prompt('Raqam kriting'));
+
+// console.log(myPrompt);
+
+// const johnWeight = +prompt("Johning massasi");
+// const johnHeight = +prompt("Johning bo`yi");
+// const bobWeight = +prompt("Bobning bo`yi");
+// const bobHeight = +prompt("Bobning bo`yi");
+
+// const johnBMI = (johnWeight / johnHeight) ** 2;
+// const bobBMI = (bobWeight / bobHeight) ** 2;
+
+
+// if (johnBMI > bobBMI) {
+//     console.log(`Jon BMI baland ${johnBMI}`)
+// } else if (bobBMI > johnBMI) {
+//     console.log(`Bob BMI baland ${bobBMI}`);
+// } else {
+//     console.log('teng');
+// }
+
+
+// for (let index = 1; index <= 100; index++) {
+//     if (index % 5 === 0 && index % 3 === 0) {
+//         console.log(`${index} Fiz Buzz`);
+//     } else if (index % 3 === 0) {
+//         console.log(`${index} Fizz`);
+//     } else if (index % 5 === 0) {
+//         console.log(`${index} Buzz`);
+//     } else {
+//         console.log('Boshqa son');
+//     }
+// }
