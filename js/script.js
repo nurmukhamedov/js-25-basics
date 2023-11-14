7b7fe7a4d2687b0ec696e5dea8a025abff53101c
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-11-14 09:59:39 +0500

    lesson-23

--- js/script.js
+++ js/script.js
@@ -1,61 +1,97 @@
-const countryUrl = 'https://restcountries.com/v3.1/all';
+const postContainer = document.querySelector('.postContainer');
 
-const searchInput = document.getElementById('searchInput');
-const form = document.getElementById('form');
-const countryWrapper = document.getElementById('countryContainer');
+async function createPost() {
+    const firstName = document.querySelector('.firstName').value.trim();
+    const lastName = document.querySelector('.lastName').value.trim();
+    const userID = new Date().getMilliseconds();
 
 
-async function getData(url) {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json;  charset=UTF-8'
+        },
+        body: JSON.stringify({ firstName, lastName, userID })
+    });
+
+
+
+    if (response.ok) {
+        const newPost = await response.json();
+        console.log(newPost);
+        const postWrapper = document.createElement('div');
+
+        const name = document.createElement('h2');
+        name.textContent = newPost.firstName;
+
+        const lastNameElement = document.createElement('h3');
+        lastNameElement.textContent = newPost.lastName;
+
+        postWrapper.appendChild(name);
+        postWrapper.appendChild(lastNameElement);
+
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        postWrapper.appendChild(editButton);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        postWrapper.appendChild(deleteButton);
+
+        postContainer.appendChild(postWrapper);
+
+        editButton.addEventListener('click', () => {
+            editPost(newPost.id, name, lastNameElement);
+        });
+
+        deleteButton.addEventListener('click', () => {
+            deletePost(newPost.id, postWrapper);
+        });
+    }
+
+}
+
+async function deletePost(postId, postElement) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+        method: 'DELETE'
+    });
+
+    if (response.ok) {
+        postContainer.removeChild(postElement);
+    } else {
+        console.log(`${postId} bilan o'chirishda xatolik ro'y berdi`);
+    }
+}
+
+async function editPost(postId, nameElement, lastNameElement) {
+    const updatedFirstName = prompt('Enter updated first name:', nameElement.textContent);
+    const updatedLastName = prompt('Enter updated last name:', lastNameElement.textContent);
+
     try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json;  charset=UTF-8'
+            },
+            body: JSON.stringify({ firstName: updatedFirstName, lastName: updatedLastName })
+        });
+
+        if (response.ok) {
+            const updatedPost = await response.json();
+            nameElement.textContent = updatedPost.firstName;
+            lastNameElement.textContent = updatedPost.lastName;
+        } else {
+            console.log(`${postId} bilan tahrirlashda xatolik ro'y berdi`);
+        }
     } catch (error) {
         console.log(error);
     }
 }
+const form = document.querySelector('.form');
 
-form.addEventListener('submit', async (event) => {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const searchValue = searchInput.value.trim().toLowerCase();
-    const countries = await getData(`https://restcountries.com/v3.1/name/${searchValue}`)
+    createPost();
 
-    renderCountry(countries);
-
-    form.reset()
-});
-
-function renderCountry(countriesData) {
-    console.log(countriesData);
-    countryWrapper.innerHTML = '';
-    countriesData.forEach((country) => {
-        const { name, flags } = country;
-        const fragment = new DocumentFragment;
-        const countryCard = document.createElement('div');
-        countryCard.classList.add('card');
-        const image = document.createElement('img');
-        image.src = flags.png;
-        image.alt = flags.alt;
-        countryCard.appendChild(image);
-        const countryName = document.createElement('h3');
-        countryName.textContent = name.common;
-        countryCard.appendChild(countryName);
-        fragment.appendChild(countryCard);
-        countryWrapper.appendChild(fragment);
-    })
-}
-getData('https://restcountries.com/v3.1/all').then((data) => renderCountry(data));
-
-
-const GITHUBAPIURL = 'https://api.github.com/users';
-
-
-const data = fetch(GITHUBAPIURL, {
-    method: 'GET',
-    headers: {
-        'Authorization': `token ${token}`,
-        'X-GitHub-Api-Version': '2022-11-28'
-    }
+    form.reset();
 })
-
-console.log(data);
